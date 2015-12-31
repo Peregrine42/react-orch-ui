076e8c24f6fa5c694c0935cf0df28ab374f3f969
Duncan Fermor <duncan@elitemail.org>
2015-12-31 10:13:23 +0000

    refactored to create instrument object

--- src/MyReactComponent.jsx
+++ src/MyReactComponent.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import promise from 'stackp/promisejs/promise.js'
 
 class TableHeader extends React.Component {
   constructor() {
@@ -18,20 +19,35 @@ class TableHeader extends React.Component {
   }
 }
 
+class Instrument {
+  constructor(data) {
+    this.id = data.id
+    this.name = data.name
+    this.amount = data.amount
+    this.reserved = data.reserved
+    this.price = data.price
+  }
+  
+  get inStock() {
+    return this.amount - this.reserved
+  }
+}
+
 class TableBody extends React.Component {
   constructor() {
     super()
   }
   
   render() {
-    let rows = this.props.rows.map((row) => {
-      return (
-        <tr key={row.id}>
-          <td>{row.name}</td>
-          <td>{row.amount}</td>
-          <td>{row.price}</td>
-        </tr>
-      )
+    let rows = this.props.rows
+      .map((row) => {
+        return (
+          <tr key={row.id}>
+            <td>{row.name}</td>
+            <td>{row.inStock}</td>
+            <td>{row.price}</td>
+          </tr>
+        )
     })
     return (
       <tbody>
@@ -44,14 +60,36 @@ class TableBody extends React.Component {
 class MyReactComponent extends React.Component {
   constructor() {
     super()
+    this.state = {
+      instruments: []
+    }
   }
-  
+  ajaxInstruments() {
+    return promise.get(
+      "http://localhost:3000/instruments.json"
+    )
+  }
+  componentDidMount() {
+    this.ajaxInstruments().then(
+      (error, data) => {
+        if (!error) {
+          let parsed = JSON.parse(data)
+          let asInstruments = parsed
+            .map((instrument) => {
+              return new Instrument(instrument)
+            })
+          this.setState({ instruments: asInstruments })
+        }
+      }
+    )
+  }
   render() {
     return (
-    <table >
-      <TableHeader/>
-      <TableBody rows={this.props.instruments}/>
-    </table>)
+      <table >
+        <TableHeader/>
+        <TableBody rows={this.state.instruments}/>
+      </table>
+    )
   }
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import promise from 'stackp/promisejs/promise.js'
 
 import MyReactComponent from './MyReactComponent.jsx!'
 
-var ajaxInstruments = () => {
-  return promise.get(
-    "http://localhost:3000/instruments.json"
-  )
-}
 
-ajaxInstruments().then(
-  (error, data) => {
-    ReactDOM.render(
-      <MyReactComponent
-        instruments={JSON.parse(data)}
-      />,
-      document.getElementById('container')
-    )
-  }
+ReactDOM.render(
+  <MyReactComponent/>,
+  document.getElementById('container')
 )
