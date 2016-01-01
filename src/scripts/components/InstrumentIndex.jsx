2455cf24d3a8b9abfd25424c59b9dc3e7ff5a893
Duncan Fermor <duncan@elitemail.org>
2016-01-01 15:59:40 +0000

    need a post function on backend

--- src/scripts/components/InstrumentIndex.jsx
+++ src/scripts/components/InstrumentIndex.jsx
@@ -9,6 +9,7 @@ class TableHeader extends React.Component {
     return (
       <thead>
         <tr>
+          <th>id</th>
           <th>name</th>
           <th>amount (in stock)</th>
           <th>price</th>
@@ -28,6 +29,7 @@ class TableBody extends React.Component {
       .map((row) => {
         return (
           <tr key={row.id}>
+            <td>{row.id}</td>
             <td>{row.name}</td>
             <td>{row.inStock}</td>
             <td>{row.price}</td>
@@ -42,6 +44,67 @@ class TableBody extends React.Component {
   }
 }
 
+class InstrumentTable extends React.Component {
+  render() {
+    return (
+      <table >
+        <TableHeader/>
+        <TableBody rows={this.props.instruments}/>
+      </table>
+    )
+  }
+}
+
+class InstrumentInspector extends React.Component {
+  render() {
+    var id = this.props.currentID
+    var actions = this.props.actions
+    var current = actions.findByID(
+      id, this.props.instruments
+    )
+    if (!(current instanceof Object) || current.error) {
+      return <div/>
+    }
+    return(
+      <div>
+        <div>
+          <label htmlFor="name">name:</label>
+          <input 
+            type="text" id="name" 
+            value={current.name}
+            onChange={actions.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="amount">amount:</label>
+          <input 
+            type="amount" id="amount"
+            value={current.amount}
+          />
+        </div>
+        <div>
+          <label htmlFor="name">reserved:</label>
+          <input 
+            type="text" id="reserved"
+            value={current.reserved}
+          />
+        </div>
+        <div>
+          <label htmlFor="inStock">in stock:</label>
+          <span> {current.inStock}</span>
+        </div>
+        <div>
+          <label htmlFor="name">description:</label>
+          <input 
+            type="text" id="description"
+            value="{{rows[activeRow].description}}"
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
 class InstrumentIndex extends React.Component {
   constructor() {
     super()
@@ -51,10 +114,16 @@ class InstrumentIndex extends React.Component {
   }
   render() {
     return (
-      <table >
-        <TableHeader/>
-        <TableBody rows={this.props.instruments}/>
-      </table>
+      <div>
+        <InstrumentTable 
+          instruments={this.props.instruments}
+        />
+      <InstrumentInspector 
+        currentID={this.props.currentID} 
+        instruments={this.props.instruments}
+        actions={this.props.actions}
+      />
+      </div>
     )
   }
 }
--- src/scripts/main.jsx
+++ src/scripts/main.jsx
@@ -8,24 +8,56 @@ import InstrumentIndex
   
 class APIStore {
   constructor() {
+    this.baseURL = 
+      "http://localhost:3000/instruments"
     this.store = Hoverboard({
-      init(state) {
-        return { 
-          instruments: [],
-        }
+      init(state, init_state) {
+        return init_state
       },
       instruments(state, data) {
         state.instruments = data
         return state
       },
       error(state, error) {
-        store.error = error
+        state.error = error
+        return state
+      },
+      handleChange(state, e) {
+        let newValue = e.target.value
+        let currentID = state.currentID
+        let target = 
+          state.actions
+            .findByID(currentID, state.instruments)
+        target.name = newValue
+        let baseURL = state.baseURL + "/"
+        let targetURL = 
+          baseURL + target.id
+        state.actions.post(targetURL, target)
         return state
       }
     })
-    this.store.init()
+    this.store.init({
+      instruments: [],
+      actions: {
+        handleChange: this.store.handleChange,
+        findByID: this.findByID,
+        post: this.post
+      },
+      currentID: 4,
+      baseURL: this.baseURL
+    })
     setInterval(this.update.bind(this), 3000)
     this.update()
+  }
+  post(url, data) {
+    promise.post(url + ".json", data)
+  }
+  findByID(id, instruments) {
+    let filtered = instruments.filter((instrument) => {
+      return id === instrument.id
+    })
+    if (filtered.length > 0) { return filtered[0] }
+    return { error: "id not found" }
   }
   validInstrument(data) {
     return {
@@ -43,7 +75,7 @@ class APIStore {
   }
   update() {
     return promise.get(
-      "http://localhost:3000/instruments.json"
+      this.baseURL + ".json"
     ).then((error, data) => {
       if (error) {
         return this.store.error(error)
@@ -57,7 +89,11 @@ class APIStore {
 let apiStore = new APIStore()
 apiStore.store.getState((props) => {
   ReactDOM.render(
-    <InstrumentIndex instruments={props.instruments}/>,
+    <InstrumentIndex 
+      currentID={props.currentID}
+      instruments={props.instruments}
+      actions={props.actions}
+    />,
     document.getElementById('container')
   )
 })
